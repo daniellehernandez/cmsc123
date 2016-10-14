65e4b99a4d34969453638572c00f2631a1e46019
Danielle Hernandez <dmhernandez@up.edu.ph>
2016-10-14 10:54:58 +0800

    Update MyList.java

--- MyList.java
+++ MyList.java
@@ -1,30 +1,13 @@
-public interface MyList {
-  
-  public void add(String item);
-  public void add(int i, String item);
-  public void set(int i, String item);
-  public String get(int i);
-  public void remove(int i);
-  public void remove(String item);
-  
- /* public void addTwo(String element){
-  
-  }
-  
-  public void addBetween(int index, String element){
-  
-  }
-  
-  public void addStart(String element){
-  
-  }
-  
-  public void addIndexEmpty(int index, String element){
-  
-  }
-  public void addIndexOutOfBounds(int index, String element){
-  
-  }
-
-*/
+public interface MyList<E> extends Iterable<E> { //1
+   public void add(E item); // 2
+   
+   public void add(int i, E item);
+   
+   public E get(int i); // 3
+   
+   public void remove(int i);
+   
+   public void set(int i, E item);
+   
+   public int size();
 }
