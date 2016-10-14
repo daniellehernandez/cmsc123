a3981a89cb6bb136b473a745202b005d83701b46
Danielle Hernandez <dmhernandez@up.edu.ph>
2016-10-14 10:52:22 +0800

    Update LListTest.java

--- LListTest.java
+++ LListTest.java
@@ -1,136 +1,88 @@
+import java.util.*;
 import junit.framework.TestCase;
 
-/**
- * A JUnit test case class.
- * Every method starting with the word "test" will be called when running
- * the test with JUnit.
- */
+
 public class LListTest extends TestCase {
-  
-  
-  private LList lst;
-  private final String[] things = {"Shampoo", "Soap", "Deodorant"};
-  private int count;
-  
-  public void setUp() {
-    lst = new LList();
-    count = 0;
-  }
-  
-  private void add(int howMany) {
-    for (int i = 0; i < howMany; i++) {
-      lst.add(things[count++]);
+ public void testNext(){
+    LList<String> list = new LList<String>();
+    list.add("Q");
+    list.add("R");
+    list.add("S");
+
+    Iterator iter = list.iterator();
+
+    assertEquals("added Q", "Q", iter.next());
+    assertEquals("added R", "R", iter.next());
+    assertEquals("added S", "S", iter.next());
     }
-  }
-  
-  
-  public void testAddOne() {
-    add(1);
-    assertEquals("added one item", "Shampoo", lst.get(0));
-  }
-  
-  
-  
-  
-  
-  
-  public void testAddTwo() {
-    add(2);
-    assertEquals("added two items and first was Shampoo", "Shampoo", lst.get(0));
-    assertEquals("added two items and second was Soap", "Soap", lst.get(1));
-  }
-  
-  public void testAddBetween() {
-    add(2);
-    lst.add(1, "Deodorant");
-    assertEquals("added two items and first was Shampoo", "Shampoo", lst.get(0));
-    assertEquals("added two items and second was Deodorant", "Deodorant", lst.get(1));
-    assertEquals("added two items and third was Soap", "Soap", lst.get(2));
-  }
-  
-  
-  public void testAddStart() {
-    add(2);
-    lst.add(0, "Deodorant");
-    assertEquals("added two items and first was Deodorant", "Deodorant", lst.get(0));
-    assertEquals("added two items and second was Shampoo", "Shampoo", lst.get(1));
-    assertEquals("added two items and third was Soap", "Soap", lst.get(2));
-  }
-  
-  public void testAddIndexEmpty() {
-    lst.add(0, "Deodorant");
-    assertEquals("added one item and first was Deodorant", "Deodorant", lst.get(0));
-  }
-  
-  public void testAddindexOutOfBounds() {
-    lst.add(0, "Deodorant");
-    try {
-      lst.add(3, "Soap");
-      fail(); // should have thrown error
-    } catch (IndexOutOfBoundsException e) {  
-      // expected!
-    }
-  }
-  
-  public void testGetOutOfBoundsWhenEmpty() {
-    LList l = new LList();
-    try {
-      l.get(0);
+
+ public void testRemove(){
+   LList<String> list = new LList<String>();
+   list.add("Q");
+   list.add("R");
+   list.add("S");
+
+   Iterator iter = list.iterator();
+   iter.next();
+   iter.remove();
+   assertEquals("removed Q", "R", iter.next());
+   iter.remove();
+   assertEquals("removed R", "S", iter.next());
+ }
+
+ public void testIllegalStateException(){
+   LList<String> list = new LList<String>();
+   Iterator iter = list.iterator();
+
+  try {
+      iter.remove();
       fail("Should have thrown an exception!");
-    } catch (IndexOutOfBoundsException e) {  
-      // expected!
+    } catch (IllegalStateException e) {
+      //
     }
-  }
-  
-  
-  public void testGetOutOfBounds() {
-    LList l = new LList();
-    l.add("Shampoo");
-    try {
-      l.get(1);
+ }
+
+ public void testISEAgain(){
+ LList<String> list = new LList<String>();
+
+
+   list.add("Q");
+   list.add("R");
+   list.add("S");
+
+  Iterator iter = list.iterator();
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+
+   iter.remove();
+
+  try {
+      iter.remove();
       fail("Should have thrown an exception!");
-    } catch (IndexOutOfBoundsException e) {  
-      // expected!
+    } catch (IllegalStateException e) {
+      //
     }
-  }
-  
-  public void testRemoveLast() {
-    LList l = new LList();
-    l.add("Shampoo");
-    l.add("Deodorant");
-    l.add("Soap");
-    l.remove(2);
-    try {
-      l.get(2);
+ }
+
+ public void testNosuchElement(){
+    LList<String> list = new LList<String>();
+   list.add("Q");
+   list.add("R");
+   list.add("S");
+
+  Iterator iter = list.iterator();
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+
+   try {
+      iter.next();
       fail("Should have thrown an exception!");
-    } catch (IndexOutOfBoundsException e) {  
-      assertEquals("added two items and first was Shampoo", "Shampoo", l.get(0));
-      assertEquals("added two items and second was Deodorant", "Deodorant", l.get(1));
+    } catch (NoSuchElementException e) {
+      //
     }
-  }
-  
-  
-  public void testRemoveMid() {
-    LList l = new LList();
-    l.add("Shampoo");
-    l.add("Deodorant");
-    l.add("Soap");
-    l.remove(1);
-    try {
-      l.get(2);
-      fail("Should have thrown an exception!");
-    } catch (IndexOutOfBoundsException e) {  
-      assertEquals("added two items and first was Shampoo", "Shampoo", l.get(0));
-      assertEquals("added two items and second was Soap", "Soap", l.get(1));
-    }
-  }
-  
-  
-  public void testSet() {
-    LList l = new LList();
-    l.add("Shampoo");
-    l.set(0, "Deodorant");
-    assertEquals("added two items and first was Deodorant", "Deodorant", l.get(0));
-  }
-  
+
+ }
+
 }
